942bee5456503d970e071f710b1407fa6a5dc409
ptmaimai106 <ptmaimai106@gmail.com>
2020-04-04 00:55:05 +0700

    Edit homepage layout

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,22 +1,52 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Header from '../containers/share/_layout/Header'
 import Footer from '../containers/share/_layout/Footer'
 import Banner from '../components/Banner'
-import EventList from '../containers/share/EventList'
+import EventList from '../containers/share/EventList';
+import EventCard from '../containers/share/EventCard';
+import { Button } from 'antd';
 
 
 
 export default class HomePage extends Component {
     render() {
         return (
-            <div>
+            <div className="homepage">
                 <Header />
                 <Banner />
-                {/* <H1>S</H1>
-                <H1>S</H1> */}
+                <div className="high-light">
+                    <h1>Sư kiện nổi bật</h1>
+                    <EventList></EventList>
+                </div>
 
-                {/* <Event></Event> */}
-                <EventList></EventList>
+                <div className="list-event">
+                    <div className="up-coming">
+                        <h1 className="">Sắp diễn ra</h1>
+                        <EventCard></EventCard>
+                        <EventCard></EventCard>
+                        <EventCard></EventCard>
+                    </div>
+
+                    <div className="latest">
+                        <h1>Mới nhất </h1>
+                        <EventCard></EventCard>
+                        <EventCard></EventCard>
+                        <EventCard></EventCard>
+                    </div>
+                </div>
+                <div className="orgnization">
+                    <h1>Một số tổ chức  </h1>
+                    <EventCard></EventCard>
+                    <EventCard></EventCard>
+                    <EventCard></EventCard>
+                </div>
+                <div className="explore">
+                    <Button size="large" type="primary">Khám phá nhiều hơn nữa !</Button>
+                </div>
+
+
+
+
                 <Footer />
             </div>
         )
