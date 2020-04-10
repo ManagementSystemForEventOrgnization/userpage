fd78aa29eb97e29f6842712c6f6ad301dd2751b9
ptmaimai106 <ptmaimai106@gmail.com>
2020-04-10 15:46:17 +0700

    Edit layout for create event page

--- src/containers/event/CreateEvent.js
+++ src/containers/event/CreateEvent.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Button } from 'antd';
-import { PlusCircleOutlined } from '@ant-design/icons'
 
 import Header from '../share/_layout/Header';
 import MenuBlockList from '../event/MenuBlockList';
@@ -11,7 +9,22 @@ class CreateEvent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            nameEvent: props.nameEvent,
+            address: props.address,
+            typeOfEvent: props.typeOfEvent,
+            quantity: props.quantity,
+            blocks: [
+                {
+                    value: props.nameEvent,
+                    style: {},
+                    key: 1,
+                },
+                {
+                    value: props.address,
+                    style: {},
+                    key: 2,
+                }
+            ]
         }
     }
 
@@ -19,14 +32,48 @@ class CreateEvent extends React.Component {
 
     }
 
+    componentDidMount = () => {
+
+    }
+
     render() {
+        const { nameEvent, address, typeOfEvent, quantity } = this.props;
+        console.log(nameEvent, address, typeOfEvent, quantity)
+        const posterStyle = {
+            width: '100%',
+            height: '60vh'
+        }
+        const addressStyle = {
+            position: 'absolute',
+            top: '40%',
+            left: '10%',
+        }
+        const typeOfEventStyle = {
+            position: 'absolute',
+            top: '71%',
+            right: '1%',
+        }
+        const nameEventStyle = {
+            position: 'absolute',
+            top: '27%',
+            left: '10%',
+            fontWeight: 'bolder'
+        }
+        const quantityStyle = {
+            position: 'absolute',
+        }
+
         return (
             <div className=" create-event">
                 <div className="fixed-top ">
                     <Header />
                 </div>
 
-                <img className="poster" alt="poster-event" src="/bg-2.jpg" />
+                <img style={posterStyle} alt="poster-event" src="/bg-2.jpg" />
+                <h1 style={nameEventStyle}>{nameEvent}</h1>
+                <h4 style={addressStyle}>{address}</h4>
+                <h5 style={quantityStyle}>Số lượng : {quantity}</h5>
+                <h6 style={typeOfEventStyle}>#{typeOfEvent}</h6>
 
                 <input type="checkbox" id="add-block" ></input>
                 <label htmlFor="add-block" className="add-block d-flex justify-content-center mt-4">
@@ -45,9 +92,9 @@ class CreateEvent extends React.Component {
 }
 
 const mapStateToProps = state => ({
-    nameEvent: state.event.nameEvent,
-    typeOfEvent: state.event.typeOfEvent,
-    address: state.event.address,
+    nameEvent: state.event.nameEvent || 'Tên sự kiện demo',
+    typeOfEvent: state.event.typeOfEvent || 'Loại sự kiện demo',
+    address: state.event.address || 'Địa chỉ demo',
     quantity: state.event.quantity
 
 })
