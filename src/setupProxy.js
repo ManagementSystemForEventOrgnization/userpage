c5bea7a6d746cbe29bdbe396e6ad57c4ef6f6701
ptmaimai106 <ptmaimai106@gmail.com>
2020-06-25 16:17:58 +0700

    Replace deployed link

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,8 @@
 const proxy = require('http-proxy-middleware').createProxyMiddleware;
 
-const proxyName = 'http://localhost:5000';
-// const proxyName = 'http://localhost:5000';
+const proxyName = 'https://event-orgnization.herokuapp.com';
 
 module.exports = function (app) {
-  //
-
   app.use(proxy('/api/joinEvent', { target: proxyName, changeOrigin: true }));
   app.use(proxy('/api/login', { target: proxyName, changeOrigin: true }));
   app.use(
