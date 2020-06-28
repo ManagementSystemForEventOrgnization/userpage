c09113682f7d2cffff5ed6696fecf1e4222ed869
Nguyen Thanh Nhut <>
2020-06-28 20:42:25 +0700

    update cònig buid

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 const proxy = require('http-proxy-middleware').createProxyMiddleware;
 
-const proxyName = process.env.SERVER_URL || 'https://event-orgnization.herokuapp.com';
+const proxyName = process.env.SERVER_URL || 'http://localhost:5000';
 
 module.exports = function (app) {
   app.use(proxy('/api/joinEvent', { target: proxyName, changeOrigin: true }));
